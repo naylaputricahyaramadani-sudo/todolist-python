ba260cb72bc97609d7ed8acad0b6a8476401b63b
naylaputricahyaramadani-sudo <naylaputricahyaramadani@gmail.com>
2026-05-08 06:04:57 +0700

    Menambahkan fitur show task

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ add_task()
 print("\nDaftar Task:")
 for t in tasks:
     print("-", t)
+
+def show_tasks():
+
+    if len(tasks) == 0:
+        print("Belum ada task.")
+        return
+
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
